000c045901810b94ad680a87ccc05eec542120e8
LQ95 <38260780+LQ95@users.noreply.github.com>
2019-01-31 12:20:57 +0100

    writing some function prototypes and a few other changes

--- Progetto/Client.c
+++ Progetto/Client.c
@@ -2,6 +2,6 @@
 
 int main(int argc,char **argv)
 {
-	ServerInit();
+	ClientInit();
 	return 0;
 }
--- Progetto/lib.h
+++ Progetto/lib.h
@@ -2,3 +2,14 @@
 #include <netinet/ip.h>
 #include <stdio.h>
 #include <stdlib.h>
+void Game();
+void ConnectToServer();
+void Signup();
+void Login();
+void ShowOnline();
+void ShowTime();
+void ShowObstacles();
+void ShowPositions();
+void ServerInit();
+void ClientInit();
+void ServerLog(char *data);
