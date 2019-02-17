443a7b44ab6affc8855fff4d74e3bc5a4f8052a6
LQ95 <38260780+LQ95@users.noreply.github.com>
2019-02-17 01:23:20 +0100

    more definitions

--- Progetto/lib.h
+++ Progetto/lib.h
@@ -2,17 +2,20 @@
 #include <netinet/ip.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
 typedef struct square{
 	int bombflag;
 }tile;
-typedef tile** Matrix;
+typedef tile **Matrix;
 typedef struct P{
 	int position[1][1];
 	int speed;
 	int score;
+	int ID;
 }Player;
-void ServerGame();
-void ClientGame();
+void ServerGame(Matrix M);
+void ClientGame(Matrix M);
 void ConnectToServer();
 void Signup();
 void Login();
