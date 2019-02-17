ba753dcaaf19e9155684f0db1d58a40a40aa7acc
LQ95 <38260780+LQ95@users.noreply.github.com>
2019-02-17 14:45:34 +0100

    More changes,starting implementation

--- Progetto/Server.c
+++ Progetto/Server.c
@@ -5,3 +5,15 @@ int main(int argc,char **argv)
 	ServerInit();
 	return 0;
 }
+
+void ServerGame(Matrix M,int time)
+{
+	int session_status,gametime;
+	gametime=time;
+	//we probably need to have a thread that handles new connections in real time here
+	while(session_status!=SESSION_END)
+	{
+		
+	}
+	
+}
--- Progetto/lib.h
+++ Progetto/lib.h
@@ -4,6 +4,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+//these are codes that the server sends the client 
+#define SESSION_END 3000
+
+//these are codes that the client sends the server in order to signal that it wants to make a move in the game 
+#define MOVE_LEFT 4900
+#define MOVE_RIGHT 5000
+#define MOVE_UP 5100
+#define MOVE_DOWN 5200
+#define NULL_MOVE 3333
+
 typedef struct square{
 	int bombflag;
 }tile;
@@ -14,7 +24,7 @@ typedef struct P{
 	int score;
 	int ID;
 }Player;
-void ServerGame(Matrix M);
+void ServerGame(Matrix M,int time);
 void ClientGame(Matrix M);
 void ConnectToServer();
 void Signup();
