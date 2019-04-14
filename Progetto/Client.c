f8b4f2791c7c9bdfd196df051250befd47b025c2
LQ95 <38260780+LQ95@users.noreply.github.com>
2019-04-14 20:29:42 +0200

    fixing a mistake

--- Progetto/Client.c
+++ Progetto/Client.c
@@ -1,5 +1,176 @@
 #include "lib.h"
 #include "scan_int/scan_int.h"
+int *UpdatePos(int *position,int moveflag)
+{
+	if (moveflag==MOVE_UP)
+	{
+		position[1]++;
+	}
+	else if (moveflag==MOVE_DOWN)
+	{
+		position[1]--;
+	}
+	else if (moveflag==MOVE_RIGHT)
+	{
+		position[0]++;
+	}
+	else if (moveflag==MOVE_LEFT)
+	{
+		position[0]--;
+	}
+	return position;
+}
+
+void print_gamepos(int width,int height,int *position)
+{
+	int x,y;
+        printf("x:%d y:%d\n",position[0],position[1]);		
+	for(x=0;x<width;x++)
+	{
+		for(y=0;y<height;y++)
+		{
+			if(x==position[0] && y==position[1])printf(".■");
+			else printf(". ");
+		}
+		printf("\n");
+	}
+}
+//the sd variable is the Client's own socket descriptor once it's been connected to the server,and the position and bomb matrices are passed by the server
+//the moveflag variable is used to signal to the client how is has mmoved if it received the MOVE_OK signal
+//the ID variable is the client's Player ID in the current game(has to be sent from the server)
+void ClientGame(int sd,int width,int height)
+{
+	int game_status,displaysize,moveflag,ID,*position;
+	game_status=LOGIN_OK;
+	char input;
+	char buf[BUFDIM];
+	char GameOverMsg[35];
+	char *answer=calloc(6,sizeof(char));
+	position=malloc(sizeof(int)*2);
+	read(sd,buf,SignalSize);
+	position[0]=atoi(buf);
+	read(sd,buf,SignalSize);
+	position[1]=atoi(buf);
+	printf("ricevo:%d %d",position[0],position[1]);
+	while(game_status!=SESSION_END)
+	{
+		printf("\nnext move:");
+		scanf(" %c",&input);
+		while ((getchar()) != '\n'); //this is used to clean stdin
+		switch(input)
+		{
+			case 'w':
+			sprintf(buf, "%d", MOVE_UP);
+			moveflag=MOVE_UP;
+			write(sd,buf,SignalSize);
+			break;
+
+			case 's':
+			sprintf(buf, "%d", MOVE_DOWN);
+			moveflag=MOVE_DOWN;
+			write(sd,buf,SignalSize);
+			break;
+
+			case 'a':
+			sprintf(buf, "%d", MOVE_LEFT);
+			moveflag=MOVE_LEFT;
+			write(sd,buf,SignalSize);
+			break;
+
+			case 'd':
+			sprintf(buf, "%d", MOVE_RIGHT);
+			moveflag=MOVE_RIGHT;
+			write(sd,buf,SignalSize);
+			break;
+
+			case 'm':
+			sprintf(buf, "%d", QUIT);
+			write(sd,buf,SignalSize);
+			break;
+
+			case '0':
+			sprintf(buf, "%d", NULL_MOVE);
+			moveflag=0;
+			write(sd,buf,SignalSize);
+			break;
+
+			default:
+			sprintf(buf, "%d", NULL_MOVE);
+			moveflag=0;
+			write(sd,buf,SignalSize);
+			break;
+		}
+		read(sd,buf,SignalSize);
+		game_status=atoi(buf);
+		printf("\nstatus %d display options:",game_status);
+		printf("\n");
+		sprintf(answer, "%d", NULL_MOVE);
+		if(game_status!=ELIMINATED && game_status!=WIN)
+		{
+			if (moveflag!=0 && game_status!=SQUARE_OCCUPIED) position=UpdatePos(position,moveflag);
+			scanf(" %c",&input);
+			while ((getchar()) != '\n');
+			switch(input)
+			{
+				case '1':
+				sprintf(buf, "%d", DISPLAY_USERS);
+				sprintf(answer, "%d", DISPLAY_USERS);
+				write(sd,buf,SignalSize);
+				break;
+
+				case '2':
+				sprintf(buf, "%d", DISPLAY_USER_LOCATIONS);
+				sprintf(answer, "%d", DISPLAY_USER_LOCATIONS);
+				write(sd,buf,SignalSize);
+				break;
+
+				case '3':
+				sprintf(buf, "%d", DISPLAY_USER_DEATHS);
+				sprintf(answer, "%d", DISPLAY_USER_DEATHS);
+				write(sd,buf,SignalSize);
+				break;
+
+
+				default:
+				sprintf(buf, "%d", NULL_MOVE);
+				sprintf(answer, "%d", NULL_MOVE);
+				write(sd,buf,SignalSize);
+				break;
+
+			}
+		if(strcmp(answer,"3000")!=0)
+			{
+				printf("\ndisplaying:");
+				read(sd,buf,DisplaySignalSize);
+				displaysize=atoi(buf);
+				read(sd,buf,displaysize) ;
+				printf("%s",buf);
+				sprintf(answer, "%d", MOVE_OK);
+			}
+		}
+	else 
+		{
+		if(game_status==ELIMINATED)
+			{
+			sprintf(GameOverMsg, "\nYou died! \n");
+			}
+		else if(game_status==WIN)
+			{
+			sprintf(GameOverMsg, "\nYou Win! \n");
+			}
+		else
+		    {
+		    sprintf(GameOverMsg, "\nGame Over \n");
+		    }
+		game_status=SESSION_END;
+		}
+	//sends and receives signals from the server,prints the map after every move as long as it participates in the game
+	printf("\n");
+	print_gamepos(width,height,position);
+	}
+printf("\n%s\n",GameOverMsg);
+return;
+}
 
 void genrcv(int sockfd)
 {
